207ea0e481c699f35a6e9a44e7bafb8bf6876fc7
Bijeyendra Prosad Pandey <118112340+Bijeyendra@users.noreply.github.com>
2022-11-13 22:31:37 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function(){
+    $('.a-slider_image').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows:false,
+        dots:true,
+      });
+});
+
+
+$(function(){
+    $('.a_testimonial_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        arrows:false,
+        dots:true,
+      });
+});
